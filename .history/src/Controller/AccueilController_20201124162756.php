ecf960a07a210195961a06980271f2840ec8dbea
Pascal Berry <ouanalao.berry@gmail.com>
2020-11-24 16:42:36 +0100

    Public
    Création des fichiers consignes, css, db, images, js puis ajout d'images, style.css et de la base de données

--- .history/src/Controller/AccueilController_20201124162756.php
+++ .history/src/Controller/AccueilController_20201124162756.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     /**
-     * @Route("/", name="accueil")
+     * @Route("/accueil", name="accueil")
      */
     public function index(): Response
     {
